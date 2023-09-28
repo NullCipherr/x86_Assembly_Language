9ed36ecb3a92f348aaf4f7a88197bca6fc27cc79
NullCipherr <blendsteal@gmail.com>
2023-09-28 10:59:12 -0300

    Update ..

--- Aula_14/Soma_Ponto_Flutuante.s
+++ Aula_14/Soma_Ponto_Flutuante.s
@@ -1,46 +1,51 @@
 .section .data
-pedido1: .asciz "\nDigite um valor (single float) => "
-mostra1: .asciz "\nValor de A = %.4f\n"
-formato1: .asciz "%f" # para simples precisao
-float1: .space 4 # aqui tbem pode ser .single
-    float2: .space 4 # aqui tbem pode ser .single
+
+	Input: .asciz "\nDigite um valor (Single Float) => "
+	Output: .asciz "\nValor de A = %.4f\n"
+	format: .asciz "%f" # Simples Precisão
+	float1: .space 4 
+    float2: .space 4 
 
 .section .text
 
 .globl _start
 
 _start:
+	finit
+	
+	# Imprime a mensagem de entrada
+	pushl $Input
+	call printf
 
-finit
+	# Recebe o valor do valor flutuante 1
+	pushl $float1
+	pushl $format
+	call scanf
+	addl $12, %esp
 
-pushl $pedido1
-call printf
+	# Imprime a mensagem de entrada
+    	pushl $Input
+	call printf
+	
+	# Recebe o valor do valor flutuante 2
+	pushl $float2
+	pushl $format
+	call scanf
+	addl $12, %esp
 
-pushl $float1
-pushl $formato1
-call scanf
-addl $12, %esp
+	# Empilha e Soma
+	flds float1
+    	flds float2
+    	faddp
 
-    pushl $pedido1
-call printf
-
-pushl $float2
-pushl $formato1
-call scanf
-addl $12, %esp
-
-flds float1
-    flds float2
-    faddp
-
-subl $8, %esp
-
-fstl (%esp)
-
-pushl $mostra1
-call printf
-addl $4, %esp
-
-    pushl $0
-   
-    call exit
+	subl $8, %esp
+	fstl (%esp)
+	
+	# Imprime a mensagem de saída com o valor da soma.	
+	pushl $Output
+	call printf
+	addl $4, %esp
+    	pushl $0
+   	
+	# Encerra o programa
+    	call exit
